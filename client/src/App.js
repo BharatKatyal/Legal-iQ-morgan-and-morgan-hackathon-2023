383dbd0c113bc483108c117a16300f76a0e0b7c5
Zander Preston <zander@pop-os.localdomain>
2023-04-28 23:30:01 -0400

    Set up for chatbot

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Chat from './pages/Chat'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/chat" />} />
+                <Route path="/chat" index element={<Chat/>} />
+            </Routes>
+        </BrowserRouter>
   );
 }
 
